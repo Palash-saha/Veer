8925c85716700c90c2e42f8c95f0f67c6660e367
palash saha <palashsaha.dev@gmail.com>
2021-08-15 14:30:18 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Veer
+
+Veer PSD TO HTML Project
+
+     Live-Site Link
+
+https://veer-ps.netlify.app
---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  // ====VENOBOX====
+  $('.venobox').venobox();
+});
+
+// ====COUNTER UP====
+jQuery(document).ready(function($) {
+  $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+});
